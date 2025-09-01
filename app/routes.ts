6870c4c3edd0949ce7ab4e743698523cbf907f11
Muhammad Rohail Omer <77564698+Racta19@users.noreply.github.com>
2025-09-01 22:06:32 +0100

    Routing

--- app/routes.ts
+++ app/routes.ts
@@ -2,7 +2,8 @@ import { type RouteConfig, layout, route } from "@react-router/dev/routes";
 
 export default [
     layout('/routes/admin/admin-layout', [
-        route(/*Path:*/'dashboard', /*File:*/ 'routes/admin/dashboard.tsx') //this is how you route using rotue in next.js
+        route(/*Path:*/'dashboard', /*File:*/ 'routes/admin/dashboard.tsx'), //this is how you route using rotue in next.js
+        route(/*Path:*/'all-users', /*File:*/ 'routes/admin/all-users.tsx')
     ]),
 
 ] satisfies RouteConfig;
