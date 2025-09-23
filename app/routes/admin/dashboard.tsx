41e3a4e11ade8430a75459a8e0b394d43b9a9236
Muhammad Rohail Omer <77564698+Racta19@users.noreply.github.com>
2025-09-23 16:32:50 +0100

    Trip implemented moving to backend (appwrite) functionality

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,8 +1,14 @@
 import Header from 'components/Header'
-import React from 'react'
+import StatsCard from 'components/StatsCard'
+import TripCard from 'components/TripCard'
+import { dashboardStats, user, allTrips } from '~/constants'
+
+
 
 const dashboard = () => {
-  const user = { name: 'Rojail'}
+  
+  const {totalUser, usersJoined, totalTrips, tripsCreated, userRole} = dashboardStats
+
   return (
     <main className='dashboard wrapper'>
       
@@ -11,9 +17,26 @@ const dashboard = () => {
         description="Track activity, trends and popular desitinations in real time" 
       />
 
-      Dashboard Page Contents
-    </main>
+      {/*Dashboard Page Contents*/}
+      <section className='flex flex-col gap-6'>
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full'>
+          <StatsCard headerTitle="Total Users" total={totalUser} currentMonthCount={usersJoined.currentMonth} lastMonthCount={usersJoined.lastMonth} />
+          <StatsCard headerTitle="Total Trips" total={totalTrips} currentMonthCount={tripsCreated.currentMonth} lastMonthCount={tripsCreated.lastMonth} />
+          <StatsCard headerTitle="Active Users" total={userRole.total} currentMonthCount={userRole.currentMonth} lastMonthCount={userRole.lastMonth} />
+        </div>
+      </section>
+
+      <section className='container'>
+        <h1 className='text-xl font-semibold text-dark-100'>Created Trips</h1>
+        <div className='trip-grid'>
+          {allTrips.slice(0,4).map(({id, name, imageUrls, itinerary, tags, estimatedPrice}) => (
+            <TripCard key={id} id={id.toString()} name={name} imageUrl={imageUrls[0]} location={itinerary?.[0]?.location ?? ''} tags={tags} price={estimatedPrice}/>
+          ))}
+        </div>
+      </section>
+     
+    </main> 
   )
-}
+} 
 
 export default dashboard
